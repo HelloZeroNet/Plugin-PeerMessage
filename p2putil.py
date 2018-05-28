6ab11c3c1efd90bb6ba3bacc406d6943e91f80dc
Ivanq <imachug@yandex.ru>
2018-05-28 20:45:09 +0300

    Send messages to merger sites

--- p2putil.py
+++ p2putil.py
@@ -1,3 +1,24 @@
+try:
+    from MergerSite import MergerSitePlugin
+    has_merger_plugin = True
+except ImportError:
+    has_merger_plugin = False
+
+
 def getWebsockets(site):
-    websockets = [ws for ws in site.websockets if "peerReceive" in ws.channels]
+    # First, site's own websockets
+    websockets = site.websockets[:]
+
+    # Now merger site
+    if has_merger_plugin:
+        merger_sites = MergerSitePlugin.merged_to_merger.get(site.address, [])
+        for merger_site in merger_sites:
+            if merger_site.address == site.address:
+                continue
+            websockets += merger_site.websockets
+
+    # Filter out sites not supporting P2P
+    # (e.g. ZeroHello, which joins all channels automatically)
+    websockets = [ws for ws in websockets if "peerReceive" in ws.channels]
+
     return websockets
