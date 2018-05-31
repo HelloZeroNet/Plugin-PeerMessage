6c3ec021714d0ae05c76f690777c4335d6493581
Ivanq <imachug@yandex.ru>
2018-05-31 12:49:46 +0300

    More background stuff

--- BackgroundPeerMessage.py
+++ BackgroundPeerMessage.py
@@ -17,4 +17,37 @@ def module(io):
 			f = UiWebsocketPlugin.actionPeerBroadcast.__get__(ws, UiWebsocketPlugin)
 			f(*args, **kwargs)
 
+		def peerSend(self, *args, **kwargs):
+			class SimulatedUiWebsocket(object):
+				def __init__(self):
+					self.site = io["site"]
+
+			ws = SimulatedUiWebsocket()
+
+			import UiWebsocketPlugin
+			f = UiWebsocketPlugin.peerSend.__get__(ws, UiWebsocketPlugin)
+			f(*args, **kwargs)
+
+		def peerInvalid(self, *args, **kwargs):
+			class SimulatedUiWebsocket(object):
+				def __init__(self):
+					self.site = io["site"]
+
+			ws = SimulatedUiWebsocket()
+
+			import UiWebsocketPlugin
+			f = UiWebsocketPlugin.peerInvalid.__get__(ws, UiWebsocketPlugin)
+			f(*args, **kwargs)
+
+		def peerValid(self, *args, **kwargs):
+			class SimulatedUiWebsocket(object):
+				def __init__(self):
+					self.site = io["site"]
+
+			ws = SimulatedUiWebsocket()
+
+			import UiWebsocketPlugin
+			f = UiWebsocketPlugin.peerValid.__get__(ws, UiWebsocketPlugin)
+			f(*args, **kwargs)
+
 	return PeerMessage()
