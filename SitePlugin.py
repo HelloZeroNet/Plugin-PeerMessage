0935dccb671e19e95e79ada401f890dcb35182fb
caryoscelus <caryoscelus@gmx.com>
2019-10-06 21:10:50 +0000

    minor code improvement: fix super/__init__ usage
    __init__ should only ever return None

--- SitePlugin.py
+++ SitePlugin.py
@@ -5,11 +5,9 @@ from Plugin import PluginManager
 @PluginManager.registerTo("Site")
 class SitePlugin(object):
     def __init__(self, *args, **kwargs):
-        res = super(SitePlugin, self).__init__(*args, **kwargs)
+        super(SitePlugin, self).__init__(*args, **kwargs)
         self.p2p_received = []
         self.p2p_result = {}
         self.p2p_to = {}
         self.p2p_unread = []
         self.p2p_last_recv = {}
-
-        return res
